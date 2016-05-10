5900d5482b4d599e771a50c70074d1362d9ad64c
Lukas Böhm <lukas.boehm93@gmail.com>
2016-05-10 23:27:09 +0200

    Add eslint and fix index.js accordingly

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-'use strict'
+'use strict';
 
 const rollup  = require('rollup');
 const fs      = require('fs');
 const url     = require('url');
-const path    = require('path');
-const dirname = path.dirname;
-const join    = path.join;
+const dirname = require('path').dirname;
+const join    = require('path').join;
 
 const extRegex = /\.js$/;
 
@@ -17,18 +16,18 @@ const defaults = {
   root: process.cwd(),
   prefix: null,
   rebuild: 'deps-change', // or 'never' or 'always'
-  serve: false, // or 'on-compile' or true. 'on-compile' has the benefit 
-                // that the bundle which is already in memory will be 
+  serve: false, // or 'on-compile' or true. 'on-compile' has the benefit
+                // that the bundle which is already in memory will be
                 // written directly into the response
   rollupOpts: {},
-  bundleOpts: {format: 'iife'},
+  bundleOpts: { format: 'iife' },
   debug: false
 };
 
 module.exports = function createExpressRollup(options) {
   const opts = Object.assign({}, defaults);
   Object.assign(opts, options);
-  
+
   // Source directory (required)
   assert(opts.src, 'rollup middleware requires src directory.');
   const src = opts.src;
@@ -36,40 +35,49 @@ module.exports = function createExpressRollup(options) {
   const dest = opts.dest || src;
   // Optional base path for src and dest
   const root = opts.root || null;
-  
+
   // Cache for bundles' dependencies list
-  let cache = {};
-  
+  const cache = {};
+
   const middleware = function(req, res, next) {
     if (req.method !== 'GET' && req.method !== 'HEAD') {
       return next();
     }
-    
+
     let path = url.parse(req.url).pathname;
-    if (opts.prefix && 0 === path.indexOf(opts.prefix)) {
+    if (opts.prefix && path.indexOf(opts.prefix) === 0) {
       path = path.substring(opts.prefix.length);
     }
-    
+
     if (!extRegex.test(path)) {
       return next();
     }
-    
-    const jsPath = join(root, dest, path.replace(new RegExp('^' + dest), ''));
+
+    const jsPath = join(root, dest, path.replace(new RegExp(`^${dest}`), ''));
     const bundlePath = join(root, src, path
-          .replace(new RegExp('^' + dest), '')
+          .replace(new RegExp(`^${dest}`), '')
           .replace(extRegex, opts.bundleExtension));
     const bundleDir = dirname(bundlePath);
-    
-    if (debug) {
+
+    if (opts.debug) {
       log('source', bundlePath);
       log('dest', jsPath);
     }
-    
+
     if (needsRebuild()) {
       const rollupOpts = Object.assign({}, opts.rollupOpts);
       rollupOpts.entry = bundlePath;
-    } else if 
+    } else if (opts.serve === true) {
+      //
+      res.send();
+      return true;
+    }
+    return undefined;
   };
+};
+
+function needsRebuild() {
+  return true;
 }
 
 function log(key, val) {
@@ -78,6 +86,6 @@ function log(key, val) {
 
 function assert(condition, message) {
   if (!condition) {
-    throw new Error('\x07\x1B[31m' + message + '\x1B[91m');
+    throw new Error(`\x07\x1B[31m${message}\x1B[91m`);
   }
 }
